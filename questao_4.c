48fc95a322e73782c28b2ee27f6ffba6d87e97f0
DBPerlin <perlinmanuella@gmail.com>
2025-10-17 23:41:40 -0300

    Questão 4 + readme

---
+++ questao_4.c
@@ -0,0 +1,24 @@
+// QUESTÃO 4
+
+// Parte 1 ->
+
+//    10. while (b != 0) {
+//    14. b = resto;
+//    23. int m0 = m, t, q;
+//    36. if (x1 < 0)
+//    45. if (exp & 1)
+//    72. InversoModular();
+//    78. int resultado = powMod(a, x, n1);
+
+//   Fazendo a multiplicação modular: 7 * 4 mod 11 = 6
+//   Sendo 4 o inverso de 3.
+//   Valor final da congruência: 4
+
+// Parte 2 (V OU F) ->
+//   1- [V]
+//   2- [F] 
+//   3- [V]
+//   4- [F]
+//   5- [F]
+//   6- [V]
+//   7- [V]
